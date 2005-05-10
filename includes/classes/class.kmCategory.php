acf065a701290c501929427dee8cb3bad2dc2048
pivwan <pivwan@78609893-fef2-0310-825b-d04861ab7526>
2005-05-10 22:43:57 +0000

    Initial import

---
+++ includes/classes/class.kmCategory.php
@@ -0,0 +1,47 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of Kermert.
+# Copyright (c) 2005 Pierre-Yves Gillier and contributors. All rights
+# reserved.
+#
+# Kermert is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Kermert is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kermert; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# ***** END LICENSE BLOCK *****
+
+include_once(dirname(__FILE__).'/class.mysql.php');
+
+class kmCategory extends Recordset
+{
+	var $categories = array();
+
+	function kmCategory($data='',$con='')
+	{
+		if(is_array($data))
+		{
+			parent::RecordSet($data);
+		}
+		if($con!='')
+		{
+		     $this->con = $con;
+		}
+	}
+
+     function loadAll()
+     {
+
+     }
+
+}
+?>
